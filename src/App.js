9670b4e0959c4b7c0fb398e8cc22ada42e1a2214
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-01-23 14:29:07 -0800

    add hover style toggle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import MenuList from './components/MenuList';
 import { links } from './data/links'
+import React, { useState } from 'react'
 
 function App() {
+  const [hoverStyle, setHoverStyle] = useState('hidden')
+  console.log(hoverStyle, 'hover style on load')
+
+  const handleHoverStyle = () => {
+    console.log('handleHoverStyle')
+    setHoverStyle('')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hover</p>
-        <MenuList allLinks={links} />
+        <p onMouseOver={handleHoverStyle}>Hover</p>
+        <div className={hoverStyle} >
+          <MenuList allLinks={links} />
+        </div>
       </header>
     </div>
   );
